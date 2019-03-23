ae97e91db9b680ae35ba519aeea3998c63b5d84c
Ranjeet <ranjeet@aymcommerce.com>
2019-03-23 19:32:36 +0400

    changed response of country codes api

--- api/lib/country.js
+++ api/lib/country.js
@@ -5,9 +5,14 @@ const axios = require('axios')
 class Country {
   static async code() {
     // fetch fares from emirates
-    const codes = await axios
+    const codes = (await axios
       .get(`${COUNTRY_CODE_API_URI}`)
-      .then(({ data }) => data)
+      .then(({ data }) => data)).map(({ name, alpha2Code, alpha3Code }) => {
+      return {
+        name: `${name} (${alpha3Code})`,
+        code: alpha2Code.toLowerCase()
+      }
+    })
 
     return codes
   }
